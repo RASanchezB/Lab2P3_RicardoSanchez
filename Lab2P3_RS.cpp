22f51d2364bb9f5789d08b1f8bafcc14089bcfb3
RASanchezB <rasanchez@unitec.edu>
2017-10-20 14:59:55 -0600

    Ejercicio 2

--- Lab2P3_RS.cpp
+++ Lab2P3_RS.cpp
@@ -8,16 +8,18 @@ int ejercicio2();
 int ejercicio3();
 
 int main(){
-  int opcion;
+  int opcion = 0;
   while(opcion != 4){
     opcion = opciones();
     switch(opcion){
      case 1:{
-         
+          
        }
      break;
-     case 2:{}
-     
+     case 2:{
+       int suma = ejercicio2();
+       cout<<"La suma es de: "<<suma<<endl;
+       }
      break;
      case 3:{}
      
@@ -42,3 +44,25 @@ int opciones(){
 int ejercicio1(){
   
 }
+int ejercicio2(){
+  cout<<"Ejercicio 2:"<<endl;
+  cout<<"Ingrese el primer numero"<<endl;
+  int a;
+  cin >> a;
+  cout<<"Ingrese el segundo numero"<<endl;
+  int b;
+  cin >> b;
+  cout<<"Ingrese el tercer numero"<<endl;
+  int n;
+  cin >> n;
+  int suma = 0;
+  for(int i = 1; i <= n; i++){
+    if(i%a == 0 && i%b == 0){
+      suma = suma + i;
+    }
+  }
+  return suma;
+}
+int ejercicio3(){
+  
+  }
